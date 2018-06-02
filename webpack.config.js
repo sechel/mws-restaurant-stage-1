3123df513f8f17917f98c3ea36e7f2e687a2a621
Stefan Sechelmann <stefan@sechel.de>
2018-06-03 00:25:17 +0200

    add babel to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: { presets: ['env'] }
+                }
+            },
             { 
                 test: /\.css$/,
                 use: [
