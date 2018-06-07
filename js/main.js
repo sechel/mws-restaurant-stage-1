43c75e1bf5a7dff17a91ff2955bd3f7b86b038e7
Stefan Sechelmann <stefan@sechel.de>
2018-06-08 00:57:59 +0200

    set details aria label text

--- js/main.js
+++ js/main.js
@@ -186,6 +186,7 @@ const createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'view details for restaurant ' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
